7f83039be5ca6a5002d18d7f6b2d016356111d32
hafsadahir <hafsa.dahir@student.moringaschool.com>
2022-09-06 23:42:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9' -]+/g, '');
+  }
+  
+  static titleize( sentence ) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let myResult = [];
+    let Words = sentence.split( " " )
+    for ( let n = 0; n < Words.length; n++ ) {
+      if ( n == 0 ) {
+        myResult.push( this.capitalize( Words[ n ] ) )
+      } else {
+        if ( exceptions.includes( Words[ n ] ) ) {
+          myResult.push( Words[ n ] )
+        } else {
+          myResult.push( this.capitalize( Words[ n ] ) )
+        }
+      }
+
+    }
+    return myResult.join( " " );
+  }
 }
+
